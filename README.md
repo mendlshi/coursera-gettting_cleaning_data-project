3e747695e60ec69c954db9f7fbd9c6839a41b804
Jason Casden <mendlshi@users.noreply.github.com>
2014-11-23 18:37:27 -0500

    More docs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting and Cleaning Data course project
+
+This script processes accelerometer data from the Samsung Galaxy S
+smartphone, reducing the data set to mean and standard deviation data,
+adding activity and subject data, and producing a tidy data set
+containing mean sensor calculations grouped by activity and
+subject.
+
+## Dependencies
+
+* data.table
+* dplyr
+* tidyr
+
+## Code Book
+
+See the [Code Book](CodeBook.md) for more information.
--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,52 @@
-# 1. Merges the training and the test sets to create one data set
+library(data.table)
+library(dplyr)
+library(tidyr)
+
 
 # Read test and training files
+features = read.table("UCI HAR Dataset/features.txt")[,2]
 
-# Merge two data tables
+# Appropriately labels the data set with descriptive variable names
+# Extracts only the measurements on the mean and standard deviation for each measurement
+test_data = data.table(read.table("UCI HAR Dataset/test/X_test.txt", col.names=features)) %>%
+  select(matches("(mean|std)\\."))
+train_data = data.table(read.table("UCI HAR Dataset/train/X_train.txt", col.names=features)) %>%
+  select(matches("(mean|std)\\."))
 
-
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement
-
-
-# 3. Uses descriptive activity names to name the activities in the data set
+# Add Subject and Activity columns
+features = read.table("UCI HAR Dataset/features.txt")[,2]
 
 # Read activity names
+activity_labels = read.table("UCI HAR Dataset/activity_labels.txt")
 
-# Read activity association file
+# Uses descriptive activity names to name the activities in the data set
+
+# Read activity association files
+test_activities = read.table("UCI HAR Dataset/test/y_test.txt")
+train_activities = readLines("UCI HAR Dataset/train/y_train.txt")
 
 # Add column for activity name factors
+test_data = mutate(test_data, activity=activity_labels[test_activities[[1]],][,2])
+train_data = mutate(train_data, activity=activity_labels[train_activities[[1]],][,2])
 
 # Read subject association file
+test_subjects = readLines("UCI HAR Dataset/test/subject_test.txt")
+train_subjects = readLines("UCI HAR Dataset/train/subject_train.txt")
 
 # Add column for subject IDs
+test_data = mutate(test_data, subject=test_subjects)
+train_data = mutate(train_data, subject=train_subjects)
 
+# Merges the training and the test sets to create one data set
+all_data = rbind(test_data, train_data) %>%
+  arrange(activity, subject)
 
-# 4. Appropriately labels the data set with descriptive variable names
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
 
+# Aggregate by activity and subject with means
+mean_sensor_data = aggregate(. ~ activity + subject, all_data, mean)
 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+# Adjust variables names to be more descriptive and accurate
+names(mean_sensor_data) = paste("Mean", names(mean_sensor_data)[-1:-2])
 
-# Group by activity and subject with means
-
+print(mean_sensor_data)
